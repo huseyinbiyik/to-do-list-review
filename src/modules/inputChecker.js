fafbab4dfe1820d077009f8e5b945c75c7e6286c
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-05-09 22:52:34 +0300

    Fix inputcheckers naming

--- src/modules/inputChecker.js
+++ src/modules/inputChecker.js
@@ -1,17 +1,17 @@
 export default function inputBoxChecker() {
-  const toDoInputBox = document.querySelectorAll('.to-do-input');
+  const taskCheckBoxes = document.querySelectorAll(".to-do-input");
 
-  toDoInputBox.forEach((element) => {
-    element.addEventListener('click', (e) => {
-      const restoredData = JSON.parse(localStorage.getItem('todolist'));
-      const targetEl = e.target;
-      const targetId = targetEl.id;
-      if (targetEl.checked === true) {
+  taskCheckBoxes.forEach((checkbox) => {
+    checkbox.addEventListener("click", (element) => {
+      const restoredData = JSON.parse(localStorage.getItem("todolist"));
+      const targetElement = element.target;
+      const targetId = targetElement.id;
+      if (targetElement.checked === true) {
         restoredData[targetId - 1].complete = true;
       } else {
         restoredData[targetId - 1].complete = false;
       }
-      localStorage.setItem('todolist', JSON.stringify(restoredData));
+      localStorage.setItem("todolist", JSON.stringify(restoredData));
     });
   });
 }
